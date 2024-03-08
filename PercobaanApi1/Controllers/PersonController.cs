3d4d36df0a4392ec384b5fb9822a292cc0cfd52a
ListyaDwiSubagya <listyodwi062@gmail.com>
2024-03-08 16:29:11 +0700

    first

---
+++ PercobaanApi1/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+
+        [HttpGet("api/person/{id_person}")]
+        public ActionResult GetPersonById(int id_person)
+        {
+            PersonContext context = new PersonContext();
+            var person = context.ListPerson().Find(x => x.id_person == id_person);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+    }
+}
---
+++ PercobaanApi1/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PercobaanApi1.Models
+{
+    public class Person
+    {
+        public int id_person { get; set; }
+        public string nama { get; set; }
+        public string alamat { get; set; }
+        public string email { get; set; }
+
+    }
+}
